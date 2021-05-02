fdd70a048ca0e86051098c1ade0086b6a47f52cf
Kawser Habib <kawser.habib.dev@gmail.com>
2021-05-02 19:24:45 +0600

    main class, poroject entry added

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,32 @@
+import akka.actor.ActorSystem
+import akka.event.{Logging, LoggingAdapter}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import model.dao.syslog.SyslogsDao
+import service.{Config, LoadSyslogs}
+
+import scala.concurrent.ExecutionContext
+
+
+object Main extends App with Config with Routes {
+  private implicit val system: ActorSystem = ActorSystem()
+  protected implicit val executor: ExecutionContext = system.dispatcher
+  protected val log: LoggingAdapter = Logging(system, getClass)
+
+  SyslogsDao.setupDB()
+  LoadSyslogs.loadSyslogs.map(res=> println("Data loading done"))
+
+  val bindingFuture = Http()
+    .bindAndHandle(handler = logRequestResult("log")(routes)
+      , interface = httpInterface, port = httpPort)
+
+  println(s"Server online at $httpInterface:$httpPort\nPress RETURN to stop...")
+  scala.io.StdIn.readLine()
+
+  bindingFuture
+    .flatMap(_.unbind())
+    .onComplete(_ => system.terminate())
+}
+
+
