5c90a3baf4c52add236196d32e6704da0ea7b9d5
Kawser Habib <kawser.habib.dev@gmail.com>
2021-05-03 10:46:02 +0600

    again failed to write test case

--- src/test/scala/BaseServiceSpec.scala
+++ src/test/scala/BaseServiceSpec.scala
@@ -1,16 +1,17 @@
-import model.dao.syslog.BaseDao
-import model.{Histogram, Syslog}
-import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.event.{LoggingAdapter, NoLogging}
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import model.Syslog
+import model.dao.syslog.BaseDao
 import org.scalatest._
+import service.Config
 
 import java.sql.Timestamp
 import java.util.Date
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-trait BaseServiceSpec extends WordSpec with Matchers with ScalatestRouteTest with Routes with BaseDao {
-  protected val log: LoggingAdapter = NoLogging
+trait BaseServiceSpec extends WordSpec with Matchers with ScalatestRouteTest with Routes with Config with BaseDao {
+  val log: LoggingAdapter = NoLogging
 
   import driver.api._
 
--- src/test/scala/SyslogsServiceSpec.scala
+++ src/test/scala/SyslogsServiceSpec.scala
@@ -1,5 +1,11 @@
 import org.scalatest.concurrent.ScalaFutures
 
 class SyslogsServiceSpec extends BaseServiceSpec with ScalaFutures {
-
+  "Syslogs service" should {
+    "retrieve syslogs list" in {
+      Get("/logs") ~> syslogsRoute ~> check {
+        response should be(testSyslogs)
+      }
+    }
+  }
 }
