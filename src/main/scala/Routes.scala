15fdce8fde75894da06c47df3199dba9f9f0da4c
Kawser Habib <kawser.habib.dev@gmail.com>
2021-05-06 14:33:56 +0600

    add cors support

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -1,11 +1,15 @@
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import api.{ApiErrorHandler, SyslogsApi}
+import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
+
 
 trait Routes extends ApiErrorHandler with SyslogsApi{
 
-  val routes: Route =
+  val routes: Route = cors() {
     pathPrefix("api") {
       syslogsRoute
     }
+  }
+
 }
